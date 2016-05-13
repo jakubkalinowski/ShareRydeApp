720b39b3c418dc7717b41381a8caddcec7585652
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-05-12 19:50:07 -0700

    reviewActivity added

---
+++ app/src/main/java/com/example/jakubkalinowski/sharerydeapp/ReviewActivity.java
@@ -0,0 +1,33 @@
+package com.example.jakubkalinowski.sharerydeapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+
+public class ReviewActivity extends AppCompatActivity {
+
+
+    Firebase ref = new Firebase("https://shareryde.firebaseio.com/");
+    private EditText mEmailAddressEditText;
+    private EditText mReviewEditText;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Firebase.setAndroidContext(this);
+        mEmailAddressEditText = (EditText) findViewById(R.id.emailAddress_review_editText);
+        mReviewEditText = (EditText) findViewById(R.id.leaveReview_review_editText);
+        mButton = (Button) findViewById(R.id.submit_review_button);
+        
+    }
+}
